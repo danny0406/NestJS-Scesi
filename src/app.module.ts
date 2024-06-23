28c16f275c5bd3305477b9005a41895735f2f75b
danny0406 <kevinpoma312@gmail.com>
2024-06-23 12:59:31 -0400

    ADD rest User

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './user/user.module';
+import { getTypeOrmConfig } from './config/database.config'; // Importa la configuración
 
 @Module({
-  imports: [ConfigModule.forRoot(
-    {
+  imports: [
+    ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
-      expandVariables: true,// Admite la expansión variable del entorno
-    }
-  )],
+      expandVariables: true, 
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => getTypeOrmConfig(configService),
+      inject: [ConfigService],
+    }),
+    UserModule, 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
